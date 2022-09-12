31ef925b43cb6625cfba3dc0f10b81fed3a38bf2
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-12 23:14:52 +0300

    adding name testing function

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,19 @@
-console.log("hi")
+const Name=document.getElementById("name")
+const MAIL=document.getElementById("email")
+const PN=document.getElementById("number")
+const Comment=document.getElementById("message")
+const Send= document.getElementById("send-btn")
+
+Send.addEventListener("click", submit)
+
+function submit(){
+    if (Name.value.length<5){
+        Name.style.borderBlockColor="red"
+    }else{
+        Name.style.borderBlockColor="#2c3e50"
+    }
+
+    if (MAIL.value.indexOf("@")=-1){
+        console.log("hi")
+    }
+}
