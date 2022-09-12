1ab7ac08933eeff8501ec3dbbe2cc3dcb27ff4bd
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-12 23:57:50 +0300

    adding message condition

--- javascript/script.js
+++ javascript/script.js
@@ -7,17 +7,17 @@ const Send= document.getElementById("send-btn")
 Send.addEventListener("click", submit)
 
 function submit(){
-    // if (Name.value.length<5){
-    //     Name.style.borderBlockColor="red"
-    // }else{
-    //     Name.style.borderBlockColor="#2c3e50"
-    // }
+    if (Name.value.length<5){
+        Name.style.borderBlockColor="red"
+    }else{
+        Name.style.borderBlockColor="#2c3e50"
+    }
 
     // if (MAIL.value.indexOf("@")=-1){
     //     console.log("hi")
     // }
+
     let pn=PN.value
-   console.log(pn.substring(0,4))
    if (pn.substring(0,4)=='+961'){
     if(pn.slice(4).length>8 & pn.slice(4).length<10){
         PN.value=PN.value
@@ -29,4 +29,9 @@ function submit(){
     PN.style.borderBlockColor="red"
    }
     
+   if (Comment.value.length <100){
+    Comment.style.borderBlockColor="red"
+   }else{
+    Comment.style.borderBlockColor="#2c3e50"
+   }
 }
