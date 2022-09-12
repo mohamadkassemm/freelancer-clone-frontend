76749ec4646e979ebd47c370ab94f02988a006c3
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-12 23:53:24 +0300

    adding phone number conditions

--- javascript/script.js
+++ javascript/script.js
@@ -7,13 +7,26 @@ const Send= document.getElementById("send-btn")
 Send.addEventListener("click", submit)
 
 function submit(){
-    if (Name.value.length<5){
-        Name.style.borderBlockColor="red"
-    }else{
-        Name.style.borderBlockColor="#2c3e50"
-    }
+    // if (Name.value.length<5){
+    //     Name.style.borderBlockColor="red"
+    // }else{
+    //     Name.style.borderBlockColor="#2c3e50"
+    // }
 
-    if (MAIL.value.indexOf("@")=-1){
-        console.log("hi")
+    // if (MAIL.value.indexOf("@")=-1){
+    //     console.log("hi")
+    // }
+    let pn=PN.value
+   console.log(pn.substring(0,4))
+   if (pn.substring(0,4)=='+961'){
+    if(pn.slice(4).length>8 & pn.slice(4).length<10){
+        PN.value=PN.value
+        PN.style.borderBlockColor="#2c3e50"
+    }else{
+        PN.style.borderBlockColor="red"
     }
+   }else{
+    PN.style.borderBlockColor="red"
+   }
+    
 }
