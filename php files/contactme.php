36fdb6036bc0bc1805bb6019708e4294f543437c
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-13 23:42:36 +0300

    adding addition of user

--- php files/contactme.php
+++ php files/contactme.php
@@ -1,6 +1,13 @@
 <?php
 include("connect.php");
 
+$name= $_POST["full-name"]
+$email= $_POST["email"]
+$phoneNumber= $_POST["phone-number"]
+$messages= $_POST["messages"]
 
+$query = "INSERT INTO articles(name, author) VALUE (" . $name .", ?)";
 
 ?>
+
+
